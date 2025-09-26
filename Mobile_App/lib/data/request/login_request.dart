11ccd49ff6004438fdc34e0258de1c321f28b16d
Amr Saleh <amrsalehma@gmail.com>
2025-09-26 22:01:36 +0300

    add logic for two screens login and signup

--- Mobile_App/lib/data/request/login_request.dart
+++ Mobile_App/lib/data/request/login_request.dart
@@ -1,30 +1,40 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:eye_app/data/model/login_response_model.dart';
 import '../model/login_model.dart';
 
 class LoginRequest {
   final Dio _dio;
+  String baseUrl = 'https://70523df67e2d.ngrok-free.app';
+  static late bool success;
+  LoginResponseModel? loginResponseModel;
 
   LoginRequest(this._dio);
 
-  Future<void> loginRequest(LoginModel request) async {
+  Future<String> loginRequest(LoginModel request) async {
     try {
       final response = await _dio.post(
-        "https://example.com/api/login",
+        "$baseUrl/api/login",
         options: Options(
           headers: {
-            "Authorization": "YOUR_TOKEN", // token
+            "Accept": "application/json", // token
           },
         ),
         data: request.toJson(),
       );
-
       log("Login Success: ${response.data}");
+      success = true;
+      loginResponseModel = LoginResponseModel.fromjson(response.data);
+      return loginResponseModel!.token;
     } on DioException catch (e) {
       if (e.response != null) {
+        success = false;
         log("Error: ${e.response?.statusCode} - ${e.response?.data}");
+        throw Exception(e.message);
       } else {
+        success = false;
         log("Error: ${e.message}");
+        throw Exception(e.message);
       }
     }
   }
